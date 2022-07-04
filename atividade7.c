ccf31771c378f6c98f01e05acdae0c3faaf2df79
lucassena7 <90392891+lucassena7@users.noreply.github.com>
2022-07-04 16:37:07 -0300

    Add files via upload

---
+++ atividade7.c
@@ -0,0 +1,179 @@
+/* Desenvolver uma função que, dada uma lista encadeada L do tipo TLista, exiba os seus elementos
+   “de trás para frente”; ou seja, o elemento apontado por L será o último a ser exibido.
+
+Nota: por ser uma atividade que avalia a manipulação de listas encadeadas, nenhuma outra
+	  estrutura de dados (como vetores, por exemplo) pode ser utilizada nesta solução. */
+
+//importação de bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+//definição de constantes
+#define TRUE 1
+#define FALSE 0
+
+//definição de tipos
+typedef struct No {
+	int valor;
+	struct No* prox;
+} TNo;
+
+typedef TNo* TLista;  //ou     typedef *TNo TLista;
+
+//protótipos das funções
+int menu ();
+int inserir (TLista *L, int numero);
+void exibir (TLista L);
+void exibirContrario (TLista L);
+
+void main ()
+{
+	//declaração de variáveis
+	TLista lista = NULL;
+	int opcao, num1, num2, resp;
+	
+	do
+	{
+		//o menu de opções será apresentado ao usuário até que ele opte por sair do sistema
+		opcao = menu ();
+		
+		//testando a opção escolhida
+		switch (opcao)
+		{
+			//Inserção
+			case 1: printf ("Numero a ser inserido: ");
+					scanf ("%d", &num1);
+			
+					//chamando a função
+					if (inserir (&lista, num1) == TRUE)
+					{
+						printf ("\n\tInsercao realizada com sucesso!\n");
+					}
+					else
+					{
+						printf ("\n\tInsercao nao realizada!\n");
+					}
+					break;
+			
+			//Exibição
+			case 2: //chamando a função
+			        exibir (lista);
+			        break;
+			       
+			//Exibição
+			case 3: //chamando a função
+			        exibirContrario (lista);
+			        break;      
+			       
+			//saída do programa 
+			case 4: printf ("Fim do programa!\n");
+			        break;
+			        
+			//opção inválida
+			default:printf ("Opcao invalida! Tente novamente.\n");
+			        break;
+		}
+		
+		system ("pause");
+	}
+	while (opcao != 4);	  //4 - opção de saída
+}
+
+// Implementação das demais funções
+int menu ()
+{
+	//declaração de vairáveis
+	int op;
+	
+	//limpando a tela
+	system ("cls");
+	
+	//exibindo o menu de opções ao usuário	
+	printf ("Menu de opcoes:\n\n");
+	printf ("(1) Inserir.\n");
+	printf ("(2) Exibir\n");
+	printf ("(3) Exibir elementos de tras para frente.\n");
+	printf ("(4) Sair\n\n");
+	
+	//lendo a opção desejada
+	printf ("Entre com a sua opcao: ");
+	scanf ("%d", &op);
+	
+	//retornando a opção
+	return op;
+}
+
+int inserir (TLista* L, int numero)
+{
+	//declaração de variáveis
+	TLista aux;
+	
+	//posso 1: alocar memória
+	aux = (TLista) malloc (sizeof(TNo)); // malloc: argumento -> quantos bytes voce quer ? aux vai guardar a quantidade de memória de TNo (retorna *void)
+	
+	//verificando se a memória foi alocada
+	if (aux == NULL)
+	{
+		return FALSE;
+	}
+	
+	else
+	{
+		//passo 2: armazenando 'numero' na posição alocada
+		aux ->valor = numero;
+		
+		//passo 3: fazendo o novo nó apontar para a aquele que até então era o primeiro nó da lista
+		aux->prox = *L;
+		
+		//passo 4: fazer com que a lista aponte para o novo elemento (uma vez que está sendo inserido no início da lista)
+		*L = aux;
+		
+		return TRUE;
+	}
+}
+
+void exibir (TLista L)
+{
+	//declaração de variáveis
+	TLista aux = L; //fazendo 'aux' apontar, inicialmente, para o primeiro nó (ou seja, aquele apontado por 'L')
+	
+	if (L == NULL)
+	{
+		printf ("Lista Vazia!");
+	}
+	
+	else
+	{
+		printf ("Lista: ");
+		
+		while (aux != NULL) //enquanto 'aux' for diferente de NULL; ou seja, enquanto existir nó na lista.
+		{
+			//printf("%d ", ((*aux).valor));
+			printf("%d ", aux->valor); //exibindo o valor que está no nó apontado por 'aux'
+			
+			aux = aux->prox; //fazendo com que 'aux' aponte para o próximo nó da lista
+		}
+	}
+}
+void exibirContrario (TLista L)
+{
+	//declaração de variáveis
+	TLista aux = L; //fazendo 'aux' apontar, inicialmente, para o primeiro nó (ou seja, aquele apontado por 'L')
+	
+	if (L == NULL)
+		{
+			printf ("Lista Vazia!");
+		}
+	
+	else
+	{
+		printf ("Lista: ");
+		
+		while (aux != NULL) //enquanto 'aux' for diferente de NULL; ou seja, enquanto existir nó na lista
+		{
+			aux = aux->prox;
+			//printf ("%d ", aux->valor);		//por esse motivo, os números são apresentados em ordem crescente, de 'numero' a 1
+		}
+		printf ("%d ", aux->valor);
+	}
+}
---
+++ atividade8.c
@@ -0,0 +1,209 @@
+/* Dadas duas listas encadeadas L1 e L2, ambas do tipo TLista, ordenadas crescentemente e sem repetição de elementos, criar uma terceira lista L3, contendo 
+   todos os elementos de L1 e L2. Como as duas originais, a nova lista deve ter seus elementos ordenados crescentemente, e sem repetição de valores. */
+   
+//importação de bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+//definição de constantes
+#define TRUE 1
+#define FALSE 0
+
+//definição de tipos
+typedef struct No {
+	int valor;
+	struct No* prox;
+} TNo;
+
+typedef TNo* TLista;  //ou     typedef *TNo TLista;
+
+//protótipos das funções
+int inserir (TLista *L, int numero);
+void exibir (TLista L);
+TLista buscar (TLista L, int numero);
+int menu ();
+
+//main
+void main ()
+{
+	//declaração de variáveis
+	TLista L1 = NULL;
+	TLista L2 = NULL;
+	int num1, num2, op;
+	
+	do
+	{
+		system ("CLS");		//limpar tela    clrscr();
+		
+		//exibindo o meu ao usuário
+		op = menu ();
+		
+		//verificando a opção escolhida
+		switch (op)
+		{
+			//Inserir na lista 1
+			case 1: printf ("\nEntre com o valor a ser inserido: ");
+			        scanf ("%d", &num1);
+			        
+			        //chamando a função
+			        if (inserir (&L1, num1) == TRUE)
+			        {
+			        	printf ("\n\tInsercao realizada com sucesso!");
+					}
+					else
+					{
+						printf ("\n\tERRO: insercao nao realizada!");
+					}
+					break;
+
+			//Inserir na lista 2
+			case 2: printf ("\nEntre com o valor a ser inserido: ");
+			        scanf ("%d", &num1);
+			        
+			        //chamando a função
+			        if (inserir (&L2, num1) == TRUE)
+			        {
+			        	printf ("\n\tInsercao realizada com sucesso!");
+					}
+					else
+					{
+						printf ("\n\tERRO: insercao nao realizada!");
+					}
+					break;
+
+			//Exibir lista1
+			case 3: exibir (L1);
+					break;
+			
+			//Exibir lista2
+			case 4: exibir (L2);
+					break;
+			
+			//Chamando a função.
+			case 5: 
+					
+			//Saída
+			case 6: printf ("\n\nPrograma finalizado!");
+			        break;
+			        
+			default: printf ("\n\nOpcao invalida! Tente novamente.");
+		}
+		
+		system ("PAUSE");
+	}
+	while (op != 6);
+}
+
+
+//implementação das funções
+int inserir (TLista *L, int numero)
+{
+	//declaração de variáveis
+	TLista aux;
+	
+	//verificando se o valor já existe na lista
+	if (buscar (*L, numero) != NULL)
+	{
+		return FALSE;
+	}
+	else
+	{	
+		//Passo 1: alocar memória para o novo nó
+		aux = (TLista) malloc (sizeof(TNo));
+		
+		//verificando se a memória foi alocada
+		if (aux == NULL)
+		{
+			return FALSE;
+		}
+		else
+		{
+			//Passo 2: armazenar 'numero' no novo nó
+			aux->valor = numero;
+			
+			//Passo 3: fazer o campo 'prox' do novo nó apontar para o "até então" primeiro nó da lista
+			aux->prox = *L;
+			
+			//Passo 4: fazer '*L' apontar para o novo nó
+			*L = aux;
+			
+			return TRUE;
+		}
+	}
+}
+
+TLista buscar (TLista L, int numero)
+{
+	//declaração de variáveis
+	TLista aux;
+	
+	//fazer com que 'aux' aponte para o primeiro nó da lista
+	aux = L;
+		
+	while (aux != NULL) //while (aux)
+	{
+		//verificando se o valor apontado por 'aux' é o número sendo buscado
+		if (aux->valor == numero)
+		{
+			return aux;			
+		}
+						
+		//atualizar o 'aux', de forma que aponte para o próximo nó
+		aux = aux->prox;
+	}
+	
+	//elemento não existe na lista
+	return NULL;
+}
+
+void exibir (TLista L)
+{
+	//declaração de variáveis
+	TLista aux;
+	
+	//verificando se a lista está vazia
+	if (L == NULL)   //if (!L)
+	{
+		printf ("Lista vazia!\n");
+	}
+	else
+	{
+		printf ("Elementos da lista: ");
+		
+		//fazer com que 'aux' aponte para o primeiro nó da lista
+		aux = L;
+		
+		while (aux != NULL) //while (aux)
+		{
+			//exibindo cada elemento da lista
+			printf ("%d ", aux->valor);
+			
+			//atualizar o 'aux', de forma que aponte para o próximo nó
+			aux = aux->prox;
+		}
+		
+		printf ("\n");	
+	}
+}
+
+int menu ()
+{
+	//declaração de variáveis
+	int opcao;
+	
+	//exibindo o meu ao usuário
+	printf ("Menu de Operacoes:\n\n");
+	printf ("(1) Inserir elementos na lista 1\n");
+	printf ("(2) Inserir elementos na lista 2\n");
+	printf ("(3) Exibir elementos da lista 1\n");
+	printf ("(4) Exibir elementos da lista 2\n");
+	printf ("(5) Criar uma terceira lista 'L3' com os elementos de 'L1' e 'L2' ordenados crescentemente e sem repeticao.\n");
+	printf ("(6) Sair\n\n");
+	
+	//lendo a opção do usuário
+	printf ("Entre com a sua opcao: ");
+	scanf ("%d", &opcao);
+	
+	//retornando a opção escolhida
+	return opcao;
+}
